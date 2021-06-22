d6544019b813a046835b7671d26698adcd6feaee
Sofia <82476902+ImNotSofia@users.noreply.github.com>
2021-06-21 23:19:06 -0300

    port change

--- index.js
+++ index.js
@@ -99,7 +99,7 @@ app.use('/', routes() );
 
 const host = process.env.HOST || '0.0.0.0';
 
-const port = process.env.PORT || 9000;
+const port = process.env.PORT || 3000;
 
 app.listen(port, host, () => {
 
